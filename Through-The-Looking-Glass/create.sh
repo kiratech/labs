ce9beffb307c3874e92fe7a9e7f0595abafd0005
Raoul Scarazzini <rasca@mmul.it>
2024-11-27 17:03:00 +0100

    [TTLG] Make script more general
    Leave aside argocd bits.

--- Through-The-Looking-Glass/create.sh
+++ Through-The-Looking-Glass/create.sh
@@ -1,4 +1,10 @@
-cat <<EOF > kind-argo-config.yml
+#!/bin/bash
+
+CTLP="ctlplane"
+TEST="test"
+PROD="prod"
+
+cat <<EOF > kind-${CTLP}-config.yml
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 networking:
@@ -9,7 +15,7 @@ nodes:
   image: kindest/node:v1.29.2@sha256:51a1434a5397193442f0be2a297b488b6c919ce8a3931be0ce822606ea5ca245
 EOF
 
-cat <<EOF > kind-test-config.yml
+cat <<EOF > kind-${TEST}-config.yml
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 networking:
@@ -20,7 +26,7 @@ nodes:
   image: kindest/node:v1.29.2@sha256:51a1434a5397193442f0be2a297b488b6c919ce8a3931be0ce822606ea5ca245
 EOF
 
-cat <<EOF > kind-prod-config.yml
+cat <<EOF > kind-${PROD}-config.yml
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 networking:
@@ -31,19 +37,19 @@ nodes:
   image: kindest/node:v1.29.2@sha256:51a1434a5397193442f0be2a297b488b6c919ce8a3931be0ce822606ea5ca245
 EOF
 
-kind create cluster --name argo --config kind-argo-config.yml
-kind create cluster --name test --config kind-test-config.yml
-kind create cluster --name prod --config kind-prod-config.yml
+kind create cluster --name ${CTLP} --config kind-${CTLP}-config.yml
+kind create cluster --name ${TEST} --config kind-${TEST}-config.yml
+kind create cluster --name ${PROD} --config kind-${PROD}-config.yml
 
-for K8S in argo test prod; do
-  kubectl --context kind-$K8S get nodes; echo
+export METALLB_VERSION='v0.14.8'
+for K8S in ${CTLP} ${TEST} ${PROD}; do
+  kubectl config use-context kind-${K8S}
   kubectl get configmap kube-proxy -n kube-system -o yaml | sed -e "s/strictARP: false/strictARP: true/" | kubectl apply -f - -n kube-system
-  export METALLB_VERSION='v0.14.8'
   kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/${METALLB_VERSION}/config/manifests/metallb-native.yaml
   kubectl wait --namespace metallb-system --for=condition=ready pod --selector=app=metallb --timeout=90s
 done
 
-cat <<EOF > kind-argo-metallb-pools.yml
+cat <<EOF > kind-${CTLP}-metallb-pools.yml
 apiVersion: metallb.io/v1beta1
 kind: IPAddressPool
 metadata:
@@ -63,7 +69,7 @@ spec:
   - mypool
 EOF
 
-cat <<EOF > kind-test-metallb-pools.yml
+cat <<EOF > kind-${TEST}-metallb-pools.yml
 apiVersion: metallb.io/v1beta1
 kind: IPAddressPool
 metadata:
@@ -83,7 +89,7 @@ spec:
   - mypool
 EOF
 
-cat <<EOF > kind-prod-metallb-pools.yml
+cat <<EOF > kind-${PROD}-metallb-pools.yml
 apiVersion: metallb.io/v1beta1
 kind: IPAddressPool
 metadata:
@@ -103,29 +109,31 @@ spec:
   - mypool
 EOF
 
-for K8S in argo test prod; do
-  kubectl apply -f kind-$K8S-metallb-pools.yml
+for K8S in ${CTLP} ${TEST} ${PROD}; do
+  kubectl config use-context kind-${K8S}
+  kubectl apply -f kind-${K8S}-metallb-pools.yml
 done
 
-kubectl config use-context kind-argo
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
 
-kubectl config use-context kind-test
-helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
-kubectl -n monitoring expose service prometheus-kube-prometheus-prometheus --name=prometheus-kube-prometheus-prometheus-lb --type=LoadBalancer
-PROMETHEUS_PROM_TEST=$(kubectl -n monitoring get svc prometheus-kube-prometheus-prometheus-lb -o jsonpath='{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}')
-echo ${PROMETHEUS_PROM_TEST}
-kubectl -n monitoring expose service prometheus-kube-prometheus-alertmanager --name=prometheus-kube-prometheus-alertmanager-lb --type=LoadBalancer
-PROMETHEUS_ALERT_TEST=$(kubectl -n monitoring get svc prometheus-kube-prometheus-alertmanager-lb -o jsonpath='{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}')
-echo ${PROMETHEUS_ALERT_TEST}
+for K8S in ${CTLP} ${TEST} ${PROD}; do
+  kubectl config use-context kind-${K8S}
+  helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
+  kubectl -n monitoring expose service prometheus-kube-prometheus-prometheus --name=prometheus-kube-prometheus-prometheus-lb --type=LoadBalancer
+  eval "PROMETHEUS_PROM_${K8S}=$(kubectl -n monitoring get svc prometheus-kube-prometheus-prometheus-lb -o jsonpath='{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}')"
+  kubectl -n monitoring expose service prometheus-kube-prometheus-alertmanager --name=prometheus-kube-prometheus-alertmanager-lb --type=LoadBalancer
+  eval "PROMETHEUS_ALERT_${K8S}=$(kubectl -n monitoring get svc prometheus-kube-prometheus-alertmanager-lb -o jsonpath='{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}')"
+done
 
-kubectl config use-context kind-prod
-helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
-kubectl -n monitoring expose service prometheus-kube-prometheus-prometheus --name=prometheus-kube-prometheus-prometheus-lb --type=LoadBalancer
-PROMETHEUS_PROM_PROD=$(kubectl -n monitoring get svc prometheus-kube-prometheus-prometheus-lb -o jsonpath='{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}')
-echo ${PROMETHEUS_PROM_PROD}
-kubectl -n monitoring expose service prometheus-kube-prometheus-alertmanager --name=prometheus-kube-prometheus-alertmanager-lb --type=LoadBalancer
-PROMETHEUS_ALERT_PROD=$(kubectl -n monitoring get svc prometheus-kube-prometheus-alertmanager-lb -o jsonpath='{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}')
-echo ${PROMETHEUS_ALERT_PROD}
+kubectl config use-context kind-${CTLP}
+kubectl -n monitoring expose deployment prometheus-grafana --type=LoadBalancer --name=prometheus-grafana-lb
+CTLP_GRAFANA_UI=$(kubectl -n monitoring get svc prometheus-grafana-lb -o jsonpath='{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}')
+
+echo "Grafana web interface: ${CTLP_GRAFANA_UI}"
+for K8S in ${CTLP} ${TEST} ${PROD}; do
+  echo -n "(${K8S}) Prometheus prometheus-kube-prometheus-prometheus-lb: "
+  eval "echo \${PROMETHEUS_PROM_${K8S}}"
+  echo -n "(${K8S}) Prometheus prometheus-kube-prometheus-alertmanager-lb: "
+  eval "echo \${PROMETHEUS_ALERT_${K8S}}"
+done
