f9640a593a3c0d6d6656f5acf87cc289ad8d0aea
Raoul Scarazzini <rasca@mmul.it>
2024-12-31 11:26:26 +0100

    [TTLG] Add log level to scripts
    Without explicit log level info logs will not be sent.

--- Through-The-Looking-Glass/send-logs-to-Loki-via-curl.sh
+++ Through-The-Looking-Glass/send-logs-to-Loki-via-curl.sh
@@ -1,3 +1,3 @@
 curl -v -H "Content-Type: application/json" \
   -s -X POST "http://172.18.0.104:8080/loki/api/v1/push" \
-  --data-raw "{\"streams\": [{ \"stream\": { \"foo\": \"bar2\" }, \"values\": [ [ \"$(date +%s%N)\", \"fizzbuzz\" ] ] }]}"
+  --data-raw "{\"streams\": [{ \"stream\": { \"application\": \"manual\", \"level\": \"info\" }, \"values\": [ [ \"$(date +%s%N)\", \"Log sent using curl\" ] ] }]}"
