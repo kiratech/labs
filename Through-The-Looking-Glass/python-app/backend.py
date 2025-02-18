ed8c8e24c902fffb27c6930105b5a41e363fa4a7
Raoul Scarazzini <rasca@mmul.it>
2025-02-18 18:39:41 +0100

    [TTLG] Add metrics to Python app
    This adds metrics to the backend Python app and the scraping part of Prometheus.

--- Through-The-Looking-Glass/python-app/backend.py
+++ Through-The-Looking-Glass/python-app/backend.py
@@ -1,6 +1,9 @@
 import os
+import time
+import random
 import traces
 import logs
+import metrics
 import variables
 from flask import Flask, request
 
@@ -15,17 +18,30 @@ traces.init_flask(app)
 # Set up Loki logs
 logger = logs.init(variables.LOGS_URL, variables.APP_BACKEND_NAME)
 
+# Set up Prometheus metrics (with /metrics endpoint)
+metrics.init_metrics(app)
+
 # Define the /process path for the application
 @app.route("/process")
 def process_request():
     client_ip = request.remote_addr
 
+    # Track the start time
+    start_time = time.time()
+
     with trace_provider.start_as_current_span(variables.APP_BACKEND_NAME):
         trace_id = trace_span.get_current_span().get_span_context().trace_id
-        message = f"Backend: Processing request from '{client_ip}' source"
-        logger.info(f"{message}", extra={"tags": {"trace_id": f"{trace_id:032x}"}})
-        return "Processed data in Backend!"
+
+    # Metrics simulate CPU time
+    time.sleep(random.uniform(0, 3))
+    duration = time.time() - start_time
+    metrics.record_request(f"{trace_id:032x}", duration)
+        
+    # Logs
+    message = f"Backend: Processing request from '{client_ip}' source"
+    logger.info(f"{message}", extra={"tags": {"trace_id": f"{trace_id:032x}"}})
+    return "Processed data in Backend!"
 
 # Execute Flask app
 if __name__ == "__main__":
-    app.run(debug=True, port=variables.APP_BACKEND_PORT)
+    app.run(debug=True, host=variables.APP_BACKEND_HOST, port=variables.APP_BACKEND_PORT)
