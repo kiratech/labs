23644dc7f0a8789a178a5a9ad76ad8a124566809
Raoul Scarazzini <rasca@mmul.it>
2025-07-11 15:52:35 +0200

    [TTLG] Fix and update helm installations
    This fixes the helm installation script to be launched from anywhere and it also
    updates the port of the tempo-query-fronted service pointed by grafana.
    
    Signed-off-by: Raoul Scarazzini <rasca@mmul.it>

--- Through-The-Looking-Glass/helm/helm-install-services.sh
+++ Through-The-Looking-Glass/helm/helm-install-services.sh
@@ -6,6 +6,8 @@ CTLP="ctlplane"
 TEST="test"
 PROD="prod"
 
+WORKDIR=$(dirname "$0")
+
 ##############
 # Prometheus #
 ##############
@@ -21,13 +23,13 @@ helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm
 helm repo update
 
 for K8S in ${TEST} ${PROD}; do
-  echo; echo "### Install Prometheus in kind-${K8S} ###"; echo
+  echo; echo "### Install Prometheus on kind-${K8S} ###"; echo
   kubectl config use-context kind-${K8S}
   # Install
   helm install prometheus prometheus-community/kube-prometheus-stack \
     --namespace monitoring \
     --create-namespace \
-    --values helm-prometheus-federates.yml
+    --values ${WORKDIR}/helm-prometheus-federates.yml
   # Expose
   kubectl -n monitoring expose service prometheus-kube-prometheus-prometheus \
     --name=prometheus-kube-prometheus-prometheus-lb \
@@ -40,7 +42,7 @@ kubectl config use-context kind-${CTLP}
 helm install prometheus prometheus-community/kube-prometheus-stack \
   --namespace monitoring \
   --create-namespace \
-  --values helm-prometheus-ctlplane.yml
+  --values ${WORKDIR}/helm-prometheus-ctlplane.yml
 # Expose
 kubectl -n monitoring expose service prometheus-kube-prometheus-prometheus \
   --name=prometheus-kube-prometheus-prometheus-lb \
@@ -60,11 +62,12 @@ kubectl -n monitoring expose service prometheus-kube-prometheus-prometheus \
 # Tempo #
 #########
 
+echo; echo "### Install Tempo on kind-${CTLP} ###"; echo
 # Install
 helm install tempo grafana/tempo-distributed \
   --namespace tempo \
   --create-namespace \
-  --values helm-tempo-ctlplane.yml
+  --values ${WORKDIR}/helm-tempo-ctlplane.yml
 # Expose
 kubectl -n tempo expose service tempo-distributor \
   --name=tempo-distributor-lb \
@@ -74,11 +77,12 @@ kubectl -n tempo expose service tempo-distributor \
 # Loki #
 ########
 
+echo; echo "### Install Loki on kind-${CTLP} ###"; echo
 # Install
 helm install loki grafana/loki \
   --namespace loki \
   --create-namespace \
-  -f helm-loki-ctlplane.yml
+  -f ${WORKDIR}/helm-loki-ctlplane.yml
 # Expose
 kubectl -n loki expose deployment loki-gateway \
   --name=loki-gateway-lb \
@@ -88,11 +92,12 @@ kubectl -n loki expose deployment loki-gateway \
 # OTEL-Collector #
 ##################
 
+echo; echo "### Install OTEL Collector on kind-${CTLP} ###"; echo
 # Install
 helm install otel-collector open-telemetry/opentelemetry-collector \
         --create-namespace \
         --namespace otel-collector \
-        -f helm-opentelemetry-collector.yml
+        -f ${WORKDIR}/helm-opentelemetry-collector.yml
 # Expose
 kubectl -n otel-collector expose service otel-collector-opentelemetry-collector \
   --name otel-collector-lb \
@@ -102,6 +107,7 @@ kubectl -n otel-collector expose service otel-collector-opentelemetry-collector 
 # Assign services vars #
 ########################
 
+echo; echo "### Get variables ###"; echo
 for K8S in ${TEST} ${PROD}; do
   kubectl config use-context kind-${K8S}
   eval "PROMETHEUS_${K8S}=$(kubectl -n monitoring get svc prometheus-kube-prometheus-prometheus-lb -o jsonpath='{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}')"
@@ -123,7 +129,7 @@ echo; echo "### Install Grafana on kind-${CTLP} ###"; echo
 helm install grafana grafana/grafana \
   --namespace grafana \
   --create-namespace \
-  -f helm-grafana-ctlplane.yml
+  -f ${WORKDIR}/helm-grafana-ctlplane.yml
 # Expose
 kubectl -n grafana patch svc grafana \
   -p '{"spec": {"type": "LoadBalancer"}}'
@@ -141,13 +147,13 @@ for K8S in ${CTLP} ${TEST} ${PROD}; do
   echo -n "(${K8S}) Prometheus: "
   eval "echo \${PROMETHEUS_${K8S}}"
 done
-echo -n "(${CTLP}) Tempo frontend:"
+echo -n "(${CTLP}) Tempo frontend: "
 eval "echo \${TEMPO_DISTRIB_${CTLP}}"
-echo -n "(${CTLP}) Loki:"
+echo -n "(${CTLP}) Loki: "
 eval "echo \${LOKI_${CTLP}}"
-echo -n "(${CTLP}) OTEL Collector:"
+echo -n "(${CTLP}) OTEL Collector: "
 eval "echo \${OTEL_COLLECTOR_${CTLP}}"
-echo -n "(${CTLP}) Grafana UI:"
-eval "echo \${GRAFANA_UI_${CTLP}}"
-echo -n "(${CTLP}) Grafana PW:"
+echo -n "(${CTLP}) Grafana UI: "
+eval "echo http://\${GRAFANA_UI_${CTLP}}"
+echo -n "(${CTLP}) Grafana PW: "
 eval "echo \${GRAFANA_PW_${CTLP}}"
