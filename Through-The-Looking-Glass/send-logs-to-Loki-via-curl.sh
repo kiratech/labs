f080ec5bc8b80fb20164f7765bd54d2597061809
Raoul Scarazzini <rasca@mmul.it>
2025-07-16 17:35:48 +0200

    [TTLG] Uniform send logs to Loki script
    This adapts the send-logs-to-Loki-via-curl.sh script so that it is similar to
    the Jaeger one.
    
    Signed-off-by: Raoul Scarazzini <rasca@mmul.it>

--- Through-The-Looking-Glass/send-logs-to-Loki-via-curl.sh
+++ Through-The-Looking-Glass/send-logs-to-Loki-via-curl.sh
@@ -1,3 +1,18 @@
-curl -v -H "Content-Type: application/json" \
-  -s -X POST "http://172.18.0.104:8080/loki/api/v1/push" \
-  --data-raw "{\"streams\": [{ \"stream\": { \"application\": \"manual\", \"level\": \"info\" }, \"values\": [ [ \"$(date +%s%N)\", \"Log sent using curl\" ] ] }]}"
+#!/bin/bash
+
+LOKI_ENDPOINT="http://172.18.0.104:8080"
+
+curl -v -s -H "Content-Type: application/json" \
+  -X POST "http://${LOKI_ENDPOINT}/loki/api/v1/push" \
+  -d "[
+    {
+      \"streams\": [
+        {
+          \"stream\": { \"application\": \"manual\", \"level\": \"info\" },
+          \"values\": [
+            [ \"$(date +%s%N)\", \"Log sent using curl\" ]
+          ]
+        }
+      ]
+    }
+  ]"
