d34c322bde2a0ef635ddbd52788a7e93b45275d8
Raoul Scarazzini <rasca@mmul.it>
2025-10-13 17:36:17 +0200

    [TTLG] Fix Loki port in python stage 2.
    Without this fix the Stage 2 would fail, since now we're relying on the
    all-in-one Loki installation (which uses 8080 instead of 3100).
    
    Signed-off-by: Raoul Scarazzini <rasca@mmul.it>

--- Through-The-Looking-Glass/python-app/Stage-2-Direct/variables.py
+++ Through-The-Looking-Glass/python-app/Stage-2-Direct/variables.py
@@ -7,4 +7,4 @@ APP_BACKEND_HOST='172.18.0.1'
 APP_BACKEND_PORT=5001
 APP_BACKEND_URL=f"http://{APP_BACKEND_HOST}:{APP_BACKEND_PORT}/process"
 TRACES_ENDPOINT='172.18.0.102:6831'                       # Tempo exposed IP
-LOGS_ENDPOINT='http://172.18.0.103:3100/loki/api/v1/push' # Loki exposed IP
+LOGS_ENDPOINT='http://172.18.0.103:8080/loki/api/v1/push' # Loki exposed IP
